cac327a2438873a9ca731a24988bbb7d3c87cda8
d21 <30766319+divC21@users.noreply.github.com>
2026-06-09 16:42:44 +0530

    Users API Delete and Patch

--- src/app.js
+++ src/app.js
@@ -7,10 +7,43 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", (req, res) => {
-  console.log(req.body);
   const user = new UserModel(req.body);
   user.save();
   res.status(200).send("User signed up successfully");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const usersList = await UserModel.find({ ...req.query });
+    if (usersList.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.status(200).send(usersList);
+    }
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
+
+app.delete("/users", async (req, res) => {
+  try {
+    const userId = req.body.id;
+    await UserModel.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
+
+app.patch("/users", async (req, res) => {
+  try {
+    const data = req.body;
+    // await UserModel.findByIdAndUpdate(data.id, data);
+    await UserModel.findOneAndUpdate({ emailId: data.emailId }, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
 });
 
 connectDB()
